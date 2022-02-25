8c96ebf127b36cba3c3fe30fefdfc05e72016e51
mahmoud <mahmoud.abuattiy106@gmail.com>
2022-02-25 06:00:21 +0200

    fix media

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
+        <Route path="/travl" exact element={<Home />} />
         <Route path='/services' element={<Services/>} />
         <Route path='/products' element={<Products />} />
         <Route path='/sign-up' element={<SignUp/>} />
--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -2,11 +2,12 @@ import React from 'react';
 import '../../App.css';
 import { Button } from '../Navbar/Button/Button';
 import './HeroSection.css';
+import heroVideo from "../../videos/video-1.mp4"
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <video src={heroVideo} autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
